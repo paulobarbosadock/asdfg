5aa975d119a8800b0f743c7794e1f7463552cc00
paulobarbosadock <paulo.barbosa@dock.tech>
2022-07-08 15:46:07 -0300

    a

--- pages/cards/[cards].tsx
+++
@@ -1,7 +0,0 @@
-import { StoplightProject } from "@stoplight/elements-dev-portal";
-
-export default function SearchCards() {
-    return (
-        <StoplightProject projectId="cHJqOjExMTUzNw" />
-    )
-}
